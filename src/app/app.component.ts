cbd67982c0b12f278702e509210cfffab99403f7
evelosog <evelosog@gmail.com>
2025-04-28 20:24:11 -0400

    Despliegue autenticación APP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,10 @@ import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 
+//Importación autenticación mediante AWS - INICIO
+import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+//Importación autenticación mediante AWS - FIN
+
 Amplify.configure(outputs);
 
 @Component({
@@ -11,8 +15,21 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, TodosComponent, 
+
+  //Importación autenticación mediante AWS - INICIO
+    AmplifyAuthenticatorModule
+  //Importación autenticación mediante AWS - FIN
+  
+],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
+    
+  //Importación autenticación mediante AWS - INICIO
+  constructor(public authenticator: AuthenticatorService) {
+    Amplify.configure(outputs);
+  }
+  //Importación autenticación mediante AWS - FIN
+  
 }
